fd62666a16cea05f634612a394b5fe697c008849
mgrinko <glukym@gmail.com>
2021-06-30 10:27:34 +0300

    implement todos loading

--- src/App.js
+++ src/App.js
@@ -1,152 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import CurrentUser from './CurrentUser';
 import TodoList from './TodoList';
-import { getTodos } from './api';
+import { getTodos, getUser } from './api';
 
-getTodos()
-  .then(todos => {
-    console.log(todos);
-  });
+const App = () => {
+  const [hasError, setError] = useState(false);
+  const [isLoading, setLoading] = useState(false);
+  const [isInitialized, setInitialized] = useState(false);
+  const [todos, setTodos] = useState([]);
+  const [currentUser, setCurrentUse] = useState(null);
 
-const todos = [
-  {
-    userId: 1,
-    id: 1,
-    title: 'delectus aut autem',
-    completed: true,
-  },
-  {
-    userId: 2,
-    id: 2,
-    title: 'quis ut nam facilis et officia qui',
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 3,
-    title: 'fugiat veniam minus',
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 4,
-    title: 'et porro tempora',
-    completed: true,
-  },
-  {
-    userId: 4,
-    id: 5,
-    title: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
-    completed: false,
-  },
-  {
-    userId: 1,
-    id: 6,
-    title: 'qui ullam ratione quibusdam voluptatem quia omnis',
-    completed: false,
-  },
-];
-const currentUser = {
-  id: 1,
-  name: 'Leanne Graham',
-  username: 'Bret',
-  email: 'Sincere@april.biz',
-  address: {
-    street: 'Kulas Light',
-    suite: 'Apt. 556',
-    city: 'Gwenborough',
-    zipcode: '92998-3874',
-    geo: {
-      lat: '-37.3159',
-      lng: '81.1496',
-    },
-  },
-  phone: '1-770-736-8031 x56442',
-  website: 'hildegard.org',
-  company: {
-    name: 'Romaguera-Crona',
-    catchPhrase: 'Multi-layered client-server neural-net',
-    bs: 'harness real-time e-markets',
-  },
+  const loadTodos = async () => {
+    setLoading(true);
+    setError(false);
+
+    try {
+      const todosFromServer = await getTodos();
+
+      setTodos(todosFromServer);
+      setInitialized(true);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const clearTodos = () => {
+    setTodos([]);
+    setInitialized(false);
+  };
+
+  return (
+    <main className="App">
+      <section>
+        <p className="info">
+          {!isInitialized && !isLoading && !hasError && <>
+            Todos are not loaded yet
+            <button type="button" onClick={loadTodos}>Load</button>
+          </>}
+
+          {isLoading && 'Loading...'}
+
+          {hasError && <>
+            Failed loading todos
+            <button type="button" onClick={loadTodos}>Reload</button>
+          </>}
+
+
+          {isInitialized && todos.length === 0 && <>
+            There are no todos
+            <button type="button" onClick={loadTodos}>Reload</button>
+          </>}
+
+
+          {isInitialized && todos.length > 0 && <>
+            {todos.length} todos are loaded
+            <button type="button" onClick={clearTodos}>Clear</button>
+          </>}
+        </p>
+
+        <p className="info">
+          User is not selected
+          Loading...
+          User #1 is loaded
+          User #999 does not exist
+          Failed loading user
+          <button type="button">Reload</button>
+        </p>
+      </section>
+
+      <section>
+        <div className="content">
+          {!isLoading && todos.length === 0 && (
+            <p>-</p>
+          )}
+
+          {isLoading && (
+            <div className="loader" />
+          )}
+
+          {!isLoading && todos.length > 0 && (
+            <TodoList todos={todos} />
+          )}
+
+        </div>
+        <div className="content">
+          {currentUser && <CurrentUser user={currentUser} />}
+        </div>
+      </section>
+    </main>
+  );
 };
-
-const App = () => (
-  <main className="App">
-    <section>
-      <p className="info">
-        Todos are not loaded yet
-        <button type="button">Load</button>
-      </p>
-
-      <p className="info">User is not selected</p>
-    </section>
-
-    <section>
-      <p className="info">Loading...</p>
-      <p className="info">Loading...</p>
-    </section>
-
-    <section>
-      <p className="info">
-        Failed loading todos
-        <button type="button">Reload</button>
-      </p>
-
-      <p className="info">
-        Failed loading user
-        <button type="button">Reload</button>
-      </p>
-    </section>
-
-    <section>
-      <p className="info">
-        6 todos are loaded
-        <button type="button">Clear</button>
-      </p>
-
-      <p className="info">
-        User #1 is loaded
-        <button type="button">Clear</button>
-      </p>
-    </section>
-
-    <section>
-      <p className="info">
-        1 todo is loaded
-        <button type="button">Clear</button>
-      </p>
-
-      <p className="info">User #999 does not exist</p>
-    </section>
-
-    <section>
-      <div className="content">
-        <p>-</p>
-      </div>
-      <div className="content">
-        <p>-</p>
-      </div>
-    </section>
-
-    <section>
-      <div className="content">
-        <div className="loader" />
-      </div>
-      <div className="content">
-        <CurrentUser user={currentUser} />
-      </div>
-    </section>
-
-    <section>
-      <div className="content">
-        <TodoList todos={todos} />
-      </div>
-      <div className="content">
-        <div className="loader" />
-      </div>
-    </section>
-  </main>
-);
 
 export default App;
--- src/store.js
+++ src/store.js
@@ -1,17 +1,37 @@
 import { createStore } from 'redux';
 
-const initialState = {};
+const initialState = {
+  x: 1,
+};
 
-const reducer = (state, action) => {
+const someAction = {
+  type: 'DO_SOMETHING',
+  payload: 'Action data',
+};
+
+const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case 'DO_SOMETHING':
+      return {
+        ...state,
+        x: state.x + 1,
+      };
+
     default:
       return state;
   }
 };
 
-const store = createStore(
-  reducer,
-  initialState
-);
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(someAction);
+store.dispatch(someAction);
+store.dispatch(someAction);
+store.dispatch(someAction);
+store.dispatch(someAction);
 
 export default store;
